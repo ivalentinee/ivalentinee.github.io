0d6847432c028fbd5a9aec81996e15cb8ec18db1
V. Ionov <valentine.emperor@gmail.com>
2019-06-07 00:47:37 +0400

    Добавил скрипт для автоматической публикации

---
+++ publish.sh
@@ -0,0 +1,11 @@
+git checkout src
+docker-compose run --rm web bundle exec jekyll build
+rm -rf /tmp/_site
+cp -rf _site /tmp/
+rm -rf *
+git checkout master
+cp -rf /tmp/_site/* ./
+git add .
+git commit -am "Published static files"
+git push -f origin master
+git checkout src
